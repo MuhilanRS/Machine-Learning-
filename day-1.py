e6c90aa63edc6f9ea6a875dfda51c0162f847d52
MuhilanRS <muhilanmuhi67@gmail.com>
2026-08-09 21:55:56 +0530

    Add files via upload

---
+++ day-1.py
@@ -0,0 +1,13 @@
+
+hours_studied = [2, 4, 7, 9]
+attendance = [60, 70, 85, 95]
+
+
+result = ["Fail", "Fail", "Pass", "Pass"]
+
+print("Features:")
+print("Hours Studied:", hours_studied)
+print("Attendance:", attendance)
+
+print("\nTarget / Label:")
+print("Result:", result)
