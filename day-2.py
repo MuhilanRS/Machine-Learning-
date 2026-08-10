20acdd67e73d0064f0716f3dd5413c589b717f0f
MuhilanRS <muhilanmuhi67@gmail.com>
2026-08-10 22:54:49 +0530

    Add files via upload

---
+++ day-2.py
@@ -0,0 +1,22 @@
+X = [
+    [2, 60],
+    [4, 70],
+    [6, 80],
+    [8, 90],
+    [10, 95]
+]
+
+
+y = [
+    "Fail",
+    "Fail",
+    "Pass",
+    "Pass",
+    "Pass"
+]
+
+print("Features (X):")
+print(X)
+
+print("\nLabels (y):")
+print(y)
